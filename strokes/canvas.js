caa9ecfec951375f15ac7a6a041aa92eb968ea04
Esteban <42194866+Estebanrg21@users.noreply.github.com>
2019-11-14 16:47:50 -0600

    Update canvas.js

--- strokes/canvas.js
+++ strokes/canvas.js
@@ -1,13 +1,19 @@
-  //**************************//
- // Code by Esteban Ramírez  //
 //**************************//
-var canvas = document.querySelector('canvas');
- pixelRat=devicePixelRatio;
+// Code by Esteban Ramírez  //
+//**************************//
+function Animacion(i) {   
+var canvas =i;
+var pixelRat = devicePixelRatio;
 canvas.width = window.innerWidth * pixelRat;
 canvas.height = window.innerHeight * pixelRat;
 var c = canvas.getContext("2d");
-c.imageSmoothingEnabled=true;
-
+c.imageSmoothingEnabled = true;
+    var circleArray = [];
+    var canti = Math.round((canvas.width) / 15);
+    let rand = (230 * (230 + 180) / 2) / 250;
+    var rangeL = canti >= 100 && canti < 200 ? rand : (canti < 100) && (canti * 5) < 180 ? canti * 5 : canti > 60 && canti < 100 ? canti * 2.7 : canti * 0.9 > 200 ? canti * 0.35 : canti * 0.9;
+    rangeL = canvas.width > 900 && pixelRat < 2 ? canvas.width > 900 && pixelRat >= 2?200:rangeL : rangeL * 4;
+    var rangeM = rangeL;  
 var mouse = {
     x: undefined,
     y: undefined
@@ -15,31 +21,48 @@ var mouse = {
 window.addEventListener('resize', () => {
     canvas.width = window.innerWidth * pixelRat;
     canvas.height = window.innerHeight * pixelRat;
+    canvas.width = window.innerWidth * pixelRat;
+    canvas.height = window.innerHeight * pixelRat;
+    var canti = Math.round((canvas.width) / 15);
+    let rand = (230 * (230 + 180) / 2) / 250;
+    var rangeL = canti >= 100 && canti < 200 ? rand : (canti < 100) && (canti * 5) < 180 ? canti * 5 : canti > 60 && canti < 100 ? canti * 2.7 : canti * 0.9 > 200 ? canti * 0.35 : canti * 0.9;
+    rangeL = canvas.width > 900 && pixelRat>1? rangeL*4 : rangeL ;
+  
+    circleArray = [];
+    for (let index = 0; index < canti; index++) {
+        var y = Math.random() * ((window.innerHeight - 8 * 2) + 8);
+        var x = Math.random() * ((window.innerWidth - 8 * 2) + 8);
+        var dx = (Math.random() - Math.random()) * pixelRat;
+        var dy = (Math.random() - Math.random()) * pixelRat;
+        var rad = Math.floor(Math.random() * ((3) - pixelRat)) + pixelRat;
+        circleArray.push(new Circle(x, y, dx, dy, rad));
+    }
 });
-window.addEventListener('mouseout',(event)=>{
-    mouse.x=undefined;
-    mouse.y=undefined;
+window.addEventListener('mouseout', (event) => {
+    mouse.x = undefined;
+    mouse.y = undefined;
 })
-window.addEventListener('mousemove', (event) => {   //Esta función es para interactuar con los circulos por la posicion en la que se encuentre el mouse
+canvas.addEventListener('mousemove', (event) => {   
     mouse.x = event.x * pixelRat;
     mouse.y = event.y * pixelRat;
 });
 
-window.addEventListener('mousedown', (event) => {
+canvas.addEventListener('mousedown', (event) => {
     mouse.x = event.x * pixelRat;
-    mouse.y = event.y *pixelRat;
-    var y = mouse.y/pixelRat;
-    var x = mouse.x/pixelRat;
+    mouse.y = event.y * pixelRat;
+    var y = mouse.y / pixelRat;
+    var x = mouse.x / pixelRat;
     var dx = (Math.random() - Math.random()) * 2;
     var dy = (Math.random() - Math.random()) * 2;
     let pr = (Math.random() * 10);
     var rad = pr <= 2.5 ? pr : 1.5;
-    if (circleArray.length<=Math.round(screen.width/10)) {
-        circleArray.push(new Circle(x, y, dx, dy, rad)); 
+    if (circleArray.length <= Math.round(window.screen.width / 10)) {
+        circleArray.push(new Circle(x, y, dx, dy, rad));
     }
 });
-var canti = Math.round(screen.width/15);//cantidad de puntos
+
 function Circle(x, y, dx, dy, rad) {
+    
     this.x = x * pixelRat;
     this.y = y * pixelRat;
     this.dx = dx * pixelRat;
@@ -48,87 +71,71 @@ function Circle(x, y, dx, dy, rad) {
     this.draw = function () {
         c.beginPath();
         c.globalAlpha = 1;
-        c.arc(this.x, this.y, this.rad, pixelRat, (Math.PI * 2)*pixelRat, false);
-        c.fillStyle = "rgba(250,250,250,1)";
-        c.fill();  
+        c.arc(this.x, this.y, this.rad, pixelRat, (Math.PI * 2) * pixelRat, false);
+        c.fillStyle = "white";
+        c.fill();
         c.save();
         c.closePath();
     }
-    
-    
     this.update = function () {
-        let punto=this;
-        
-        
-        if (punto.x + punto.rad >= canvas.width  || punto.x + punto.rad  <= 0) {
-            punto.dx = -punto.dx;
+        if (this.x + this.rad >= canvas.width || this.x + this.rad <= 0) {
+            this.dx = -this.dx;
         }
-        if (punto.y + punto.rad >= canvas.height || punto.y + punto.rad  <= 0) {
-            punto.dy = -punto.dy;
+        if (this.y + this.rad >= canvas.height || this.y + this.rad <= 0) {
+            this.dy = -this.dy;
         }
-        punto.x += punto.dx;
-        punto.y += punto.dy;
-        var rangeL= Math.sqrt(Math.pow((this.x - (this.x+100)),2)+Math.pow((this.y-(this.y+100)),2));
-       
-        
-        var rangeM =250 * pixelRat;
-        for (let index = 0; index < circleArray.length; index++) {
+        this.x += this.dx;
+        this.y += this.dy;
            
-            if (punto.x - circleArray[index].x < rangeL && punto.x - circleArray[index].x > -rangeL &&
-                punto.y - circleArray[index].y < rangeL && punto.y - circleArray[index].y > -rangeL) {
-                var op = Math.abs(punto.x - circleArray[index].x);
-                var opy = Math.abs(punto.y - circleArray[index].y);   
-                let plusOne=(op+opy)/100;
-                let opacidad =1;
-                opacidad -=plusOne/2;
+         for (let index = 0; index < circleArray.length; index++) {
+             const punto2 = circleArray[index];
+            if (this.x - punto2.x < rangeL && this.x - punto2.x > -rangeL &&
+                this.y - punto2.y < rangeL && this.y - punto2.y > -rangeL) {
+                let factor = ((Math.abs((this.x) - (punto2.x))) + (Math.abs((this.y) - (punto2.y)))) / rangeL*1.05;
+                let opacidad = (1 - factor) < 0 ? 0 : 1 - factor;
                 c.beginPath();
-                c.strokeStyle = 'rgba(255,255,255,' + Math.abs(opacidad)+')';
-                c.globalAlpha = Math.abs(opacidad) ;  
-                c.moveTo(circleArray[index].x, circleArray[index].y);
-                c.lineTo(punto.x, punto.y);
-                c.lineWidth=pixelRat;
-                
+                c.strokeStyle = 'rgba(255,255,255,' + Math.abs(opacidad) + ')';
+                c.moveTo(this.x, this.y);
+                c.lineTo(punto2.x, punto2.y);
+                c.lineWidth = opacidad*pixelRat;
                 c.stroke();
-                c.save();
                 c.closePath();
             }
         }
-        if (mouse.x - punto.x <= rangeM && mouse.x - punto.x >= -rangeM &&
-            mouse.y - punto.y <= rangeM && mouse.y - punto.y >= -rangeM) {
-            var opx = Math.abs(mouse.x - punto.x);
-            var opy1 = Math.abs(mouse.y -punto.y);
-            let plusOneM=(op+opy)/100;
-            let opacidadM =1;
-            opacidadM -=plusOneM/2;
+        if (mouse.x - this.x <= rangeM && mouse.x - this.x >= -rangeM &&
+            mouse.y - this.y <= rangeM && mouse.y - this.y >= -rangeM) {
+            let factorM = ((Math.abs((this.x) - (mouse.x))) + (Math.abs((this.y) - (mouse.y)))) / rangeL * 0.5 ;
+            let opacidadM = (1 - factorM) < 0 ? 0 : 1 - factorM;
             c.beginPath();
             c.strokeStyle = 'rgba(255,255,255,' + Math.abs(opacidadM) + ')';
-            c.globalAlpha = Math.abs(opacidadM) ;  
-            c.moveTo(punto.x, punto.y);
-            c.lineTo(mouse.x, mouse.y);
-            c.lineWidth =pixelRat;
+            c.globalAlpha = Math.abs(opacidadM);
+            c.moveTo(mouse.x, mouse.y);
+            c.lineTo(this.x, this.y);
+            c.lineWidth = pixelRat;
             c.stroke();
             c.save();
             c.closePath();
         }
-        punto.draw();
+        this.draw();
     }
 }
-var circleArray = [];
+
 for (let index = 0; index < canti; index++) {
-    var y = Math.random() * ((innerHeight - 8 * 2) + 8);
-    var x = Math.random() * ((innerWidth - 8 * 2) + 8);
-    var dx = (Math.random() * pixelRat - Math.random() * pixelRat) * pixelRat ;
-    var dy = (Math.random() * pixelRat - Math.random() * pixelRat) * pixelRat ;
-    let pr = (Math.random() * 10);
-    var rad =  pr<= 2.5 ? pr : 1.5;
+    var y = Math.random() * ((window.innerHeight - 8 * 2) + 8);
+    var x = Math.random() * ((window.innerWidth - 8 * 2) + 8);
+    var dx = (Math.random()  - Math.random() ) * pixelRat;
+    var dy = (Math.random()  - Math.random() ) * pixelRat;
+    var rad = Math.floor(Math.random() * ((3) - pixelRat)) + pixelRat;
     circleArray.push(new Circle(x, y, dx, dy, rad));
 }
 function animateit() {
-        canvas.width = canvas.width;
-        canvas.height = canvas.height; 
-       for (let index = 0; index < circleArray.length; index++) {
-        circleArray[index].update(); 
+    canvas.width = canvas.width;
+    canvas.height = canvas.height;
+    for (let i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
+     
     }
     requestAnimationFrame(animateit);
 }
 animateit();
+}
